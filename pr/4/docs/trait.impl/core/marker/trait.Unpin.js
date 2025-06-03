83f6869e481a16957a4d4415d50e0bb705bef7e8
Frando <Frando@users.noreply.github.com>
2025-06-03 12:21:24 +0000

    deploy: edecda0219bf5477470ac2ae27c15ec05de0e84a

---
+++ pr/4/docs/trait.impl/core/marker/trait.Unpin.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["n0_snafu",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"enum\" href=\"n0_snafu/enum.Error.html\" title=\"enum n0_snafu::Error\">Error</a>",1,["n0_snafu::error::Error"]],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/marker/trait.Unpin.html\" title=\"trait core::marker::Unpin\">Unpin</a> for <a class=\"struct\" href=\"n0_snafu/struct.SpanTrace.html\" title=\"struct n0_snafu::SpanTrace\">SpanTrace</a>",1,["n0_snafu::spantrace::SpanTrace"]]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[584]}
